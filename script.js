75953d38c1e70ee2959bdb7282de540c808c05d3
Wanderson Davyd <103713059+WandevPB@users.noreply.github.com>
2023-08-06 23:10:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
         strings:["Sistemas Para Internet."],
         typeSpeed:100,
         backSpeed:60,
-        loop:false
+        loop:true
     });
 
     $('.menu-btn').click(function(){
